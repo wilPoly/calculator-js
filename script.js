00d5d270521c2f9e5bc9e50633a8bd2d0e98a467
wilpoly <misterwrinkle@gmail.com>
2021-09-25 16:22:55 +0200

    Add pair calculation. Can't string calculation yet

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
-// add
 function add(a, b) {
 	return a + b;
 }
-// substract
+
 function substract(a, b) {
 	return a - b;
 }
-// multiply
+
 function multiply(a, b) {
 	return a * b;
 }
-// divide
+
 function divide(a, b) {
 	return a / b;
 }
@@ -30,10 +29,62 @@ function operate(operator, a, b) {
 	}
 }
 
-// query the #display div
-// display the values chosen by the user
-// when the = sign is chosen by the user : 
-	// display the result of the operate function inside the div
-		// get the result
-		// store it in a variable
-		// 
+const display = document.querySelector("#display");
+const buttons = document.querySelectorAll("#buttons > button");
+console.table(buttons);
+
+buttons.forEach(button => {
+	button.addEventListener("click", (e) => compose(e.target));
+});
+
+function printValue(value) {
+	display.textContent = `${value}`;
+	console.log(value, typeof value);
+}
+
+let displayValue = "0";
+let firstValue = 0;
+let secondValue = 0;
+let result = 0;
+let operation = "";
+
+function compose(button) {
+	switch (button.className) {
+		case "operator":
+			operation = button.value;
+			firstValue = Number(displayValue);
+			displayValue = "0";
+			break;
+
+		case "operand":
+			if (displayValue === "0") {
+				displayValue = button.value;
+			}
+			else {
+				displayValue += button.value;
+			}
+			printValue(displayValue);
+			break;
+
+		case "equals":
+			secondValue = Number(displayValue);
+			displayValue = operate(operation, firstValue, secondValue);
+			printValue(displayValue);
+			break;
+		
+		case "clear":
+			allClear();
+			break;
+			
+			console.log(operation);
+	}
+}
+
+function allClear() {
+	displayValue = "0";
+	firstValue = 0;
+	secondValue = 0;
+	result = 0;
+	operation = "";
+	printValue(displayValue);
+}
